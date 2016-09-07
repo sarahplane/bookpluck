05dd6519e169c5dd664b5c42649250a29f999894
Jack Plane <jgplane@gmail.com>
2016-09-06 18:58:04 -0600

    support create action, new and index pages for notecards

--- app/controllers/notecards_controller.rb
+++ app/controllers/notecards_controller.rb
@@ -7,9 +7,19 @@ class NotecardsController < ApplicationController
   end
 
   def new
+    @notecard = Notecard.new
   end
 
   def create
+    @notecard = Notecard.new(notecard_params)
+    @notecard.title = params[:notecard][:title]
+
+  if @notecard.save
+    flash[:notice] = "Notecard added"
+    redirect_to notecards_path
+  else
+    render :new
+  end
   end
 
   def edit
@@ -19,5 +29,10 @@ class NotecardsController < ApplicationController
   end
 
   def destroy
+  end
+
+private
+  def notecard_params
+    params.require(:notecard).permit(:title, :quote, :note)
   end
 end
