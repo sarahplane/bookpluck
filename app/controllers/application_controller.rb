2e66ee5e7a63b4085636420439cfd43a0083ef14
Jack Plane <jgplane@gmail.com>
2016-09-24 07:28:42 -0600

    application layout changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   protect_from_forgery with: :exception, prepend: true
+  after_filter :store_action
+
+  def store_action
+    return unless request.get?
+    if (request.path != "/users/sign_in" &&
+        request.path != "/users/sign_up" &&
+        request.path != "/users/password/new" &&
+        request.path != "/users/password/edit" &&
+        request.path != "/users/confirmation" &&
+        request.path != "/users/sign_out" &&
+        !request.xhr?) # don't store ajax calls
+      store_location_for(:user, notecards_path)
+      # redirects to notecard index after sign-in
+    end
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home], raise: false
-  
+
   def home
+    if current_user
+      redirect_to notecards_path
+    end
   end
 end
