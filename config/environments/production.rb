8376d86f9633d0ad769d8b4fdc3a2d114f53d446
Jack Plane <jgplane@gmail.com>
2016-11-29 16:58:45 -0700

    change to sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,12 +16,13 @@ Rails.application.configure do
 
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['MAILGUN_SMTP_LOGIN'],
-    :password => ENV['MAILGUN_SMTP_PASSWORD'],
-    :address => ENV['MAILGUN_SMTP_SERVER'],
-    :domain => 'bookpluck.heroku.com',
-    :port => ENV['MAILGUN_SMTP_PORT'],
-    :authentication => :plain
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'bookpluck.heroku.com',
+    :enable_starttls_auto => true
   }
 
   # Code is not reloaded between requests.
