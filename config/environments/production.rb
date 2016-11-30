f7637e06db080f46400ad344aa35655d67ac864a
Jack Plane <jgplane@gmail.com>
2016-11-29 17:29:10 -0700

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,14 +3,12 @@ Rails.application.configure do
 
   # Heroku with Mailtrap: Code from https://devcenter.heroku.com/articles/mailtrap
   require 'rubygems' if RUBY_VERSION < '1.9'
-  require 'rest_client'
+  #require 'rest_client'
   require 'json'
 
-  response = RestClient.get "https://mailtrap.io/api/v1/inboxes.json?api_token=45330ca51ed5205c6c9972da19a4b0e0"
-  #response = RestClient.get "https://api.mailgun.net/v3/appeec922fd1331450a8b3e5346abee1593.mailgun.org"
+  #response = RestClient.get "https://mailtrap.io/api/v1/inboxes.json?api_token=45330ca51ed5205c6c9972da19a4b0e0"
 
-
-  first_inbox = JSON.parse(response)[0] # get first inbox
+  #first_inbox = JSON.parse(response)[0] # get first inbox
 
   config.action_mailer.default_url_options = { :host => 'https://bookpluck.heroku.com' }
 
@@ -64,7 +62,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  #config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
