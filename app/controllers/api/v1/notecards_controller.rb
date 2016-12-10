cc3a02c7bda5f5814897086ecc161ef9c814f73c
Jack Plane <jgplane@gmail.com>
2016-12-10 14:45:40 -0700

    added authors, themes, and books to api

--- app/controllers/api/v1/notecards_controller.rb
+++ app/controllers/api/v1/notecards_controller.rb
@@ -11,11 +11,17 @@ class Api::V1::NotecardsController < Api::V1::BaseController
 
   def create
     @notecard = Notecard.new(notecard_params)
+    @notecard.book =  Book.find_or_create_by(title: params[:book_attributes][:title])
+    @user = current_user
+    assign_author
+    @notecard.theme_list=(params[:theme_list])
+
     if @notecard.save
       render json: {
         status: 201,
         message: "Successfully created notecard",
       }.to_json
+      @user.notecards << @notecard
     else
       render json: {
         status: 400,
@@ -27,6 +33,12 @@ class Api::V1::NotecardsController < Api::V1::BaseController
 
   def update
     @notecard = Notecard.find(params[:id])
+    @notecard.theme_list=(params[:theme_list]) unless params[:theme_list].nil?
+    if !params[:book_attributes].nil?
+      @notecard.book = Book.find_or_create_by(title: params[:book_attributes][:title])
+    end
+    assign_author unless params[:author_names].nil?
+
     if @notecard.update(notecard_params)
       render json: {
         status: 200,
@@ -59,6 +71,19 @@ class Api::V1::NotecardsController < Api::V1::BaseController
 private
 
   def notecard_params
-    params.require(:notecard).permit(:title, :quote, :note)
+    params.require(:notecard).permit(:title, :quote, :note, :author_names, :theme_list, book_attributes: [:title, :timestamp])
+  end
+
+  def assign_author
+    unless params[:author_names] == nil
+      author_array = []
+      authors = params[:author_names].split(',')
+      authors.each do |author|
+        first_name, last_name = author.rpartition(" ").collect(&:strip).reject!(&:empty?)
+        new_author = Author.find_or_create_by(first_name: first_name, last_name: last_name)
+        author_array << new_author
+      end
+      @notecard.authors = author_array
+    end
   end
 end
